0f42248904b7ec85a4b2e1a9e2c23bb400f50b96
Sammyh1 <sammyimhook@gmail.com>
2016-11-09 16:06:50 +0000

    Created the basic spider

--- Storagerank.py
+++ Storagerank.py
@@ -1,27 +1,42 @@
 import re
 import requests
 
-
-
 exclusion_list = []
 
-locations_to_target = ['london']
+locations_to_target = ['Birmingham', 'Brighton', 'Bristol Ashton Gate', 'Bristol Central', 'Camberley', 'Cambridge',
+                       'Canterbury', 'Cardiff', 'Cheadle and Wilmslow', 'Chelmsford', 'Cheltenham', 'Chester',
+                       'Colchester', 'Derby', 'Dundee', 'Edinburgh', 'Gloucester', 'Guildford', 'High Wycombe', 'Hull',
+                       'Leeds', 'Liverpool Edge Lane', 'Liverpool North', 'Liverpool South', 'Luton', 'Macclesfield',
+                       'Milton Keynes', 'Morecambe', 'Norwich', 'Nottingham', 'Oxford', 'Peterborough', 'Poole',
+                       'Portsmouth', 'Reading', 'Sheffield Bramall Lane', 'Sheffield Hillsborough', 'Sheffield Parkway',
+                       'Sheffield West Bar', 'Slough', 'Southend', 'Stockport', 'Stockton', 'Stoke', 'Swindon',
+                       'Tunbridge Wells', 'Warrington', 'West Molesey']
+
 
 def Main():
     for location in locations_to_target:
         goog_url = "https://www.google.co.uk/search?q=storage+"
         goog_url += location
-        result = visit_google_url(goog_url)
+        visit_google_url(goog_url, location)
 
 
-def visit_google_url(url):
+def visit_google_url(url, location):
     headers = {
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36'}
     google_page = requests.get(url, headers=headers).content
     sort_of_link = re.compile(r'<a class="rllt__action-button _Jrh" href="([^"]*)')
     iterator = sort_of_link.finditer(str(google_page))
+    big_yellow = re.compile(r'bigyellow')
+    new_list = [location]
     for match in iterator:
-        print(str(match.group(1)))
+        new_list.append(str(match.group(1)))
+
+    try:
+        if not big_yellow.search(new_list[1]):
+            print(new_list)
+
+    except:
+        pass
 
 
 '''
